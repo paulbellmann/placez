219bad6db00afd881ad000263066f4e49b522c8b
paulbellmann <paul.bellmann@yahoo.de>
2018-04-16 09:54:12 +0200

    = forces https on heroku

--- places/settings/prod.py
+++ places/settings/prod.py
@@ -11,3 +11,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # overide database settings
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
