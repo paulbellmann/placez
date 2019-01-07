9b41dea19a8a5be8835902ab42badc58afe5c8d1
paulbellmann <paul.bellmann@yahoo.de>
2019-01-07 08:38:07 +0100

    + test speed up

--- places/settings/local.py
+++ places/settings/local.py
@@ -1,3 +1,11 @@
 from .base import *
+import sys
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+TESTING = 'test' in sys.argv
+
+if TESTING:
+    PASSWORD_HASHERS = [
+        'django.contrib.auth.hashers.MD5PasswordHasher',
+    ]
