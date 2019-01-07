ca799106a466a304d7b71117dd01fb5d2c53dbc9
paulbellmann <paul.bellmann@yahoo.de>
2019-01-07 22:15:43 +0100

    +-= removed old model, tried to add a ModelForm

--- portal/models.py
+++ portal/models.py
@@ -3,31 +3,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, HiddenInput
+
 
 # Create your models here.
-
-
-class Item(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    # def __str__(self):
-    #     return "%s, %s" % (self.last_name, self.first_name)
-
-
-class ItemForm(ModelForm):
-    class Meta:
-        model = Item
-        fields = ['first_name', 'last_name']
 
 
 class Point(models.Model):
     title = models.CharField(max_length=100)
     street = models.CharField(max_length=100, blank=True)
     city = models.CharField(max_length=100)
-    # postal = models.IntegerField()
     lat = models.CharField(max_length=100, blank=True, null=True)
     lng = models.CharField(max_length=100, blank=True, null=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -39,3 +24,27 @@ class Point(models.Model):
             return "greenIcon"
         else:
             return "redIcon"
+
+    def __str__(self):
+        return "%s, %s" % (self.title, self.street)
+
+# nearly working ModelForm
+# cant quite figure out why the form is not valid
+# and how to save the owner
+class PointFormTest(ModelForm):
+    class Meta:
+        model = Point
+        fields = ['title', 'street', 'city', 'lat', 'lng', 'visited']
+        widgets = {
+            'title': TextInput(attrs={'placeholder': 'Mein Ziel'}),
+            'street': TextInput(attrs={'placeholder': 'gewuenschte Straße'}),
+            'city': TextInput(attrs={'placeholder': 'gewuenschte Stadt'}),
+            'lat': HiddenInput(),
+            'lng': HiddenInput(),
+        }
+        labels = {
+            'title': 'Titel',
+            'street': 'Straße',
+            'city': 'Stadt',
+            'visited': 'Besucht?'
+        }
